6ac0d2d4be1e271a2d892c7c6f6683c779b56c44
Danilo Peña <nicolasdpf@gmail.com>
2020-07-21 13:03:11 -0500

    New logic for opReducer

--- src/operations/opReducer.js
+++ src/operations/opReducer.js
@@ -5,83 +5,112 @@ var num = Number.concatenate;
 export const opReducer = (state, action) => {
   switch (action.type) {
     case types.addDigit:
-      if (state.currentValue === 0) {
+      if (state.display === 0 || state.isFirstNumReady)
+        return { ...state, display: action.payload, isFirstNumReady: false };
+      else
         return {
           ...state,
-          currentValue: action.payload,
+          display: Number(state.display + '' + action.payload),
+          isFirstNumReady: false,
+        };
+    case types.sum:
+      if (!state.firstNum) {
+        return {
+          ...state,
+          firstNum: state.display,
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.sum,
         };
       } else {
         return {
           ...state,
-          currentValue: Number(state.currentValue + '' + action.payload),
+          firstNum: calcTotal(state.firstNum, state.display, types.sum),
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.sum,
         };
       }
+    case types.sub:
+      if (!state.firstNum) {
+        return {
+          ...state,
+          firstNum: state.display,
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.sub,
+        };
+      } else {
+        return {
+          ...state,
+          firstNum: calcTotal(state.firstNum, state.display, types.sub),
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.sub,
+        };
+      }
+    case types.mult:
+      if (!state.firstNum) {
+        return {
+          ...state,
+          firstNum: state.display,
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.mult,
+        };
+      } else {
+        return {
+          ...state,
+          firstNum: calcTotal(state.firstNum, state.display, types.mult),
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.mult,
+        };
+      }
+    case types.div:
+      if (!state.firstNum) {
+        return {
+          ...state,
+          firstNum: state.display,
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.div,
+        };
+      } else {
+        return {
+          ...state,
+          firstNum: calcTotal(state.firstNum, state.display, types.div),
+          display: 0,
+          isFirstNumReady: true,
+          operator: types.div,
+        };
+      }
+    case types.total:
+      return {
+        ...state,
+        firstNum: null,
+        display: calcTotal(state.firstNum, state.display, state.operator),
+        operator: '=',
+      };
     case types.resetOps:
       return {
         ...state,
-        currentValue: 0,
-        partialResult: 0,
+        firstNum: null,
+        display: 0,
+        isFirstNumReady: false,
+        operator: '',
       };
     case types.delete:
       return {
         ...state,
-        currentValue: Number(state.currentValue.toString().slice(0, -1)),
+        display: Number(state.display.toString().slice(0, -1)),
       };
-    case types.floatingPt:
+    case types.percent:
       return {
         ...state,
-        currentValue: state.currentValue + '.',
-      };
-    case types.sum:
-      return {
-        ...state,
-        currentValue: 0,
-        partialResult: calcTotal(
-          state.partialResult,
-          action.currentValue,
-          types.sum
-        ),
-        lastOp: types.sum,
-      };
-    case types.sub:
-      console.log(action.payload);
-      return {
-        ...state,
-        currentValue: 0,
-        partialResult: calcTotal(
-          state.partialResult,
-          action.payload,
-          types.sub
-        ),
-        lastOp: types.sub,
-      };
-    case types.mult:
-      return {
-        ...state,
-        currentValue: 0,
-        partialResult: calcTotal(
-          state.partialResult,
-          action.payload,
-          types.mult
-        ),
-        lastOp: types.mult,
-      };
-    case types.total:
-      return {
-        ...state,
-        partialResult: 0,
-        currentValue: calcTotal(
-          state.partialResult,
-          state.currentValue,
-          state.lastOp
-        ),
+        display: state.display * 0.01,
       };
     default:
       break;
   }
 };
-
-var num = 1234;
-var num2 = parseInt(num.toString().slice(0, -1));
-
-console.log(num2);
--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,10 @@
 export const types = {
-  keyNum: 'keyNum',
-  keyFun: 'keyFun',
   addDigit: 'addDigit',
+  addFirstNum: 'addFirstNum',
+  addSecondNum: 'addSecondNum',
   resetOps: 'C',
   post2neg: '+/-',
+  percent: '%',
   delete: 'del',
   floatingPt: '.',
   sum: '+',
