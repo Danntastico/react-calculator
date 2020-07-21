6a9847a691647a198c0ff8d0efaf96f33d2cf0c4
Danilo Peña <nicolasdpf@gmail.com>
2020-07-21 13:06:23 -0500

    first build

---
+++ docs/precache-manifest.3c96c5ffbe4a39d247f94783b70c9d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d3b9b353ad799f45f8574c51718404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ead6d37e610317904ea",
+    "url": "/static/css/main.7a87efe8.chunk.css"
+  },
+  {
+    "revision": "53e4040994628718c42a",
+    "url": "/static/js/2.95927b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95927b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ead6d37e610317904ea",
+    "url": "/static/js/main.14cb1eb7.chunk.js"
+  },
+  {
+    "revision": "7aaf91a552c540c62644",
+    "url": "/static/js/runtime-main.a15bdacd.js"
+  }
+]);
