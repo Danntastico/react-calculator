6afbf3fc0079a0fe9b2ee745f02644f5035788dc
Danilo Peña <nicolasdpf@gmail.com>
2020-07-21 13:02:28 -0500

    Modified properties of KeyFunc components

--- src/containers/Keypad/Keypad.jsx
+++ src/containers/Keypad/Keypad.jsx
@@ -7,33 +7,33 @@ export const Keypad = () => {
   return (
     <div className='keypad__container'>
       <div className='keypad__row'>
-        <KeyFunc value='C' type={types.keyFun} />
-        <KeyFunc value='del' type={types.keyFun} />
-        <KeyFunc value='%' type={types.keyFun} />
-        <KeyFunc value='/' type={types.keyFun} />
+        <KeyFunc value='C' />
+        <KeyFunc value='del' />
+        <KeyFunc value='%' />
+        <KeyFunc value='/' />
       </div>
       <div className='keypad__row'>
         <KeyNum value={7} />
         <KeyNum value={8} />
         <KeyNum value={9} />
-        <KeyFunc value='X' type={types.keyFun} />
+        <KeyFunc value='X' />
       </div>
       <div className='keypad__row'>
         <KeyNum value={4} />
         <KeyNum value={5} />
         <KeyNum value={6} />
-        <KeyFunc value='-' type={types.keyFun} />
+        <KeyFunc value='-' />
       </div>
       <div className='keypad__row'>
         <KeyNum value={1} />
         <KeyNum value={2} />
         <KeyNum value={3} />
-        <KeyFunc value='+' type={types.keyFun} />
+        <KeyFunc value='+' />
       </div>
       <div className='keypad__row'>
         <KeyNum value={0} />
         <KeyFunc value='.' />
-        <KeyFunc value='=' type={types.keyFun} />
+        <KeyFunc value='=' />
       </div>
     </div>
   );
